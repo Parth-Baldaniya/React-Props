34f3e0d8fbb0ccfece810afda4a6e01f4f5ecce0
ParthBaldaniya1242 <133247721+ParthBaldaniya1242@users.noreply.github.com>
2023-07-16 22:38:25 +0530

    Add files via upload

---
+++ src/Sdata.jsx
@@ -0,0 +1,51 @@
+const Sdata = [
+  {
+    id: 1,
+    sname: "DARK",
+    imgsrc: "https://wallpapercave.com/wp/wp4056410.jpg",
+    title: "A Netflix Original Series",
+    links: "https://www.netflix.com/in/title/80990668?source=35",
+  },
+  {
+    id: 2,
+    sname: "extra curricular",
+    imgsrc:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRp273lEOgZBVzj3tSjrvkYBsssp7r78IPT9dNjIqW8gA&s",
+    title: "A Netflix Original Series",
+    links: "https://www.netflix.com/in/title/80990668?source=35",
+  },
+  {
+    id: 3,
+    sname: "Stranger Things",
+    imgsrc:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkaWKth5eI1Wx61vjkH7iOlTrujmpfPYJoJMR_JxQ&s",
+    title: "A Netflix Original Series",
+    links: "https://www.netflix.com/in/title/80990668?source=35",
+  },
+  {
+    id: 4,
+    sname: "the Vampire Diaries",
+    imgsrc:
+      "https://img.resized.co/spin1038/eyJkYXRhIjoie1widXJsXCI6XCJodHRwczpcXFwvXFxcL21lZGlhLnJhZGlvY21zLm5ldFxcXC91cGxvYWRzXFxcLzIwMjFcXFwvMDJcXFwvMDExMDEwMjNcXFwvQzZod2laWFU0QUFBSHFGLWUxNjEyMTc0MjU3NTc3LmpwZ1wiLFwid2lkdGhcIjoxMjAwLFwiaGVpZ2h0XCI6OTAwLFwiZGVmYXVsdFwiOlwiaHR0cHM6XFxcL1xcXC93d3cuc3BpbjEwMzguY29tXFxcL2ltYWdlc1xcXC9uby1pbWFnZS5wbmdcIixcIm9wdGlvbnNcIjpbXX0iLCJoYXNoIjoiODg5ZDhlYjVlOTU2MTE0ZjY0ZmExNjQxYjZiYjNjNTZkYmY4MzM1MyJ9/c6hwizxu4aaahqf-e1612174257577.jpg",
+    title: "A Netflix Original Series",
+    links: "https://www.netflix.com/in/title/80990668?source=35",
+  },
+  {
+    id: 5,
+    sname: "My First-2 Love",
+    imgsrc:
+      "https://i.pinimg.com/736x/a6/41/4f/a6414fc3dab572c5dae29306f76e159b.jpg",
+    title: "A Netflix Original Series",
+    links: "https://www.netflix.com/in/title/80990668?source=35",
+  },
+  {
+    id: 6,
+    sname: "extra curricular",
+    imgsrc:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRp273lEOgZBVzj3tSjrvkYBsssp7r78IPT9dNjIqW8gA&s",
+    title: "A Netflix Original Series",
+    links: "https://www.netflix.com/in/title/80990668?source=35",
+  },
+];
+
+export default Sdata;
